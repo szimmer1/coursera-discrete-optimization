b61db2742780ce759fcaf68a1ab7fd29579f47c1
Shahar Zimmerman <shahar@uber.com>
2017-08-06 14:53:11 -0700

    update

--- knapsack/dp.go
+++ knapsack/dp.go
@@ -4,21 +4,21 @@ package main
 // 			 and the vector of decision variables (x0...xI) in range [0,1]
 func DPSolve(k int, w []int, v []int) (int, int, []int) {
 	s := make([]int, len(w))
-	return DPSolveAux(k, len(w)-1, w, v, s), 1, s
+	return O(k, len(w)-1, w, v, s), 1, s
 }
 
 func DPSolveMemo(k int, w []int, v []int) (int, int, []int) {
 }
 
-func DPSolveAux(k int, i int, w []int, v []int, s []int) int {
+func O(k int, i int, w []int, v []int, s []int) int {
 	if i < 0 {
 		return 0
 	}
 	if k < w[i] {
-		return DPSolveAux(k, i-1, w, v, s)
+		return O(k, i-1, w, v, s)
 	} else {
-		pick := v[i] + DPSolveAux(k-w[i], i-1, w, v, s)
-		unpick := DPSolveAux(k, i-1, w, v, s)
+		pick := v[i] + O(k-w[i], i-1, w, v, s)
+		unpick := O(k, i-1, w, v, s)
 		if pick > unpick {
 			s[i] = 1
 			return pick
@@ -26,4 +26,7 @@ func DPSolveAux(k int, i int, w []int, v []int, s []int) int {
 			return unpick
 		}
 	}
+}
+
+func OMemo(k int, w []int, v []int, M [][]int) {
 }
